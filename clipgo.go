7a1cd9144846d3e83df652e333265318e24280bb
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2015-12-28 12:28:24 +0900

    指定したファイルの内容をコピー

--- clipgo.go
+++ clipgo.go
@@ -2,33 +2,49 @@ package main
 
 import (
 	"fmt"
+	"github.com/atotto/clipboard"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
-	"log"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
 var (
-	templateList = kingpin.Command("list", "help string")
-	// tempalteTag    = kingpin.Flag("tag", "Select server id to speedtest").Short('t').String()
+	templateList = kingpin.Command("list", "利用可能なテンプレートの一覧が表示されます")
+	copyCommand  = kingpin.Command("copy", "指定したタグのテンプレートをクリップボードにコピーします").Default()
+	tempalteTag  = copyCommand.Flag("tag", "クリップボードにコピーするテンプレートを指定します").Short('t').String()
 )
-
-// ./.clipgo/clipgo.json
 
 func main() {
 	kingpin.Version("0.0.1")
 	switch kingpin.Parse() {
 	case "list":
 		showTemplates()
-
+	case "copy":
+		copyTemplate(*tempalteTag)
 	}
+}
+
+func copyTemplate(tag string) {
+	bytes, err := ioutil.ReadFile("./.clipgo/templates/" + tag + ".md")
+	if err != nil {
+		notExist, _ := regexp.MatchString("no such file or directory", err.Error())
+		if notExist {
+			fmt.Println("指定したファイルは存在しません")
+		} else {
+			fmt.Println(err.Error())
+		}
+	}
+	s := string(bytes[:len(bytes)])
+	clipboard.WriteAll(s)
+	fmt.Println("クリップボードに[" + tag + ".md] の内容をコピーしました")
 }
 
 func showTemplates() {
 	files, err := ioutil.ReadDir("./.clipgo/templates")
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 	fmt.Println("登録されているテンプレートは...")
 	for _, file := range files {
