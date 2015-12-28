d2a2e7d9a5006bfb031fa1c6b52549c95ad5ce1b
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2015-12-28 13:26:06 +0900

    設定ファイルをユーザーホームに

--- clipgo.go
+++ clipgo.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/atotto/clipboard"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
+	"os/user"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -16,6 +18,10 @@ var (
 	tempalteTag  = copyCommand.Flag("tag", "クリップボードにコピーするテンプレートを指定します").Short('t').String()
 )
 
+type conf struct {
+	Path string `json:"templatePath"`
+}
+
 func main() {
 	kingpin.Version("0.0.1")
 	switch kingpin.Parse() {
@@ -26,8 +32,24 @@ func main() {
 	}
 }
 
-func copyTemplate(tag string) {
-	bytes, err := ioutil.ReadFile("./.clipgo/templates/" + tag + ".md")
+func getTemplateDir() string {
+	user, _ := user.Current()
+	configurationFile := user.HomeDir + "/.clipgo.json"
+
+	bytes, err := ioutil.ReadFile(configurationFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var c conf
+	json.Unmarshal(bytes, &c)
+
+	return c.Path
+}
+
+func copyTemplate(tag string) error {
+	templateDir := getTemplateDir()
+	bytes, err := ioutil.ReadFile(templateDir + tag + ".md")
 	if err != nil {
 		notExist, _ := regexp.MatchString("no such file or directory", err.Error())
 		if notExist {
@@ -35,14 +57,17 @@ func copyTemplate(tag string) {
 		} else {
 			fmt.Println(err.Error())
 		}
+		return err
 	}
 	s := string(bytes[:len(bytes)])
 	clipboard.WriteAll(s)
 	fmt.Println("クリップボードに[" + tag + ".md] の内容をコピーしました")
+	return nil
 }
 
 func showTemplates() {
-	files, err := ioutil.ReadDir("./.clipgo/templates")
+	templateDir := getTemplateDir()
+	files, err := ioutil.ReadDir(templateDir)
 	if err != nil {
 		fmt.Println(err)
 	}
